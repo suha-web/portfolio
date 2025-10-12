e852b6eb526253c55bb0bce71fd49d9faea2d187
suha shoohani <shoohanisuha@gmail.com>
2025-10-12 23:11:06 +0100

    initial p5.js setup

--- drawring/sketch.js
+++ drawring/sketch.js
@@ -3,9 +3,20 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255);
   
-  noStroke();
-  fill(80, 80, 80);
-  square (20, 20, 60);
+  strokeWeight (5);
+  noFill();
+  ellipse (295, 220, 30, 50);
+  
+  fill(0, 0, 0, 255);
+  circle (40, 130, 30);
+  circle (140, 130, 30);
+  circle (240, 130, 30);
+  circle (340, 130, 30);
+ 
+  strokeWeight (5);
+  line(65, 200, 115, 250);
+  line(65, 250, 115, 200);
+
 }
