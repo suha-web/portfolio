46c1a55b826825ba2faa8dd8a28568608c090b6a
suha shoohani <shoohanisuha@gmail.com>
2025-10-29 17:18:42 +0000

    initial p5.js setup

--- multiples/sketch.js
+++ multiples/sketch.js
@@ -1,50 +1,52 @@
-let row = 3;     
-let col = 3;     
+let row = 32;     
+let col = 32;     
+let pad = 4;      
+let sWidth, sHeight;  
+let rgba = [255, 255, 255, 255];     
+let numCircles = 400
 let circles = [];
+let circleToShow = 0;
 
 function setup() {
-    createCanvas(200, 200); 
-    noFill();
-    Stroke(0);
-    circles = [
-      {i: 0, j: 1},
-      {i: 1, j: 2},
-      {i: 2, j: 0},
-      {i: 2, j: 2}
-    ];
+    createCanvas(400, 400); 
+    sWidth = (width/col)-(pad+(pad/col));
+    sHeight = (height/row)-(pad+(pad/row));
+    rectMode(CENTER); 
+    noStroke();
+
+
+    for (let i = 0; i < numCircles; i++){
+      let x = random(width);
+      let y = random(height);
+      let size = random(10, 30);
+      circles.push({x: x, y: y, size: size});
+    }
 }
 
 function draw(){
-  background(255); 
- 
-  let cellW = width / col;
-  let cellH = height / row; 
-
-  stroke(120);
-  for (let i = 0; i < 30; i++){
-    beginShape();
-    for (let j = 0; j < 20; j++) {
-    vertex (random(width), random(height));
-  }
-  endShape();
-}
+  background(0); 
+   
   
-  stroke(0);
-  for (let i = 1; i < col; i++);{
-    line(i * cellW, 0, i * cellW, height);
-  }
-  for (let j = 1; j < row; j++){
-    line(0, j * cellH, width, j * cellH);
+  for(let i=0; i<row; i++){
+    for(let ii=0; ii<col; ii++){
+      let x = pad+(ii*sWidth)+(pad*ii)+(sWidth/2);
+      let y = pad+(i*sHeight)+(pad*i)+(sHeight/2);
+      fill(rgba); 
+      rect(x, y, sWidth, sHeight); 
+      
+    }
   }
 
-  fill(255, 0, 0);
-  noStroke();
-  for (let c of circles){
-    let x = c.j * cellW + cellW / 2;
-    let y = c.j * cellH + cellH / 2;
-    ellipse(x, y, 50, 50);
-    
+  fill(175, 0, 0);
+  for(let i = 0; i < circleToShow; i++){
+    circle(circles[i].x, circles[i].y, circles[i].size);
   }
+ 
+
+
+if (circleToShow < numCircles){
+  circleToShow += 1;
+}
 }
 
 
