20caa99999d6d583106421758fae2120c2f3256b
suha shoohani <shoohanisuha@gmail.com>
2025-10-31 13:12:56 +0000

    initialp5.js setup

--- media/sketch.js
+++ media/sketch.js
@@ -1,15 +1,35 @@
 let img;
+let sound;
+let project;
+let fading = false;
 
 function preload(){
   img = loadImage('IMG_2643.jpg');
+  sound = loadSound("code .m4a");
+  project = loadSound("Project-[Extract]IMG_5837.Mov.m4a")
+
 }
 
 function setup() {
   createCanvas(400, 400);
   image(img, 0, 0, 400, 400);
+
+  sound.loop();
+  project.loop();
+  
+  project.setVolume(0);
 }
 
-function draw(){
 
+function mousePressed(){
+  userStartAudio()
+  if (!fading) {
+    fading = true;
+    sound.setVolume(0,2);
+    project.setVolume(1,2);
+    
+   setTimeout(() => {isfading = false}, 3000);
+  }
 }
+
 
